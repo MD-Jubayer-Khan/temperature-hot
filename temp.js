b6c16238fa971ec6679a83e6ac76ffeb6978799f
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-04-21 12:40:40 +0600

    first commit

---
+++ temp.js
@@ -0,0 +1,9 @@
+const API_KEY = `7ca7e1db0a6a0946c519b5d3a3e69664`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
